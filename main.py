eb71a6006155f09082b27fdb15c67710c15478c2
Ryosuke-SagaU <19238346@edu.cc.saga-u.ac.jp>
2022-05-30 15:39:33 +0900

    [modify] change file name to get it easy

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from increasing_image import main
+# from increasing_image import main
 
 # Press the green button in the gutter to run the script.
+from vott_convert_json import change_json_file_name
+
 if __name__ == '__main__':
-    main()
+    change_json_file_name()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- vott_convert_json.py
+++ vott_convert_json.py
@@ -1,78 +1,15 @@
-import json
+import os
+from glob import glob
 
-file_name = './json/raw'
-class_list = {'circle':0}
+def change_json_file_name():
 
-with open(file_name) as f:
-    # 開いたファイルから、Json 形式で展開する
-    js = json.load(f)
+    # json/new_json_data 以下のファイルをリストとして出力する
+    print(glob('json/new_json_data/*'))
 
-    # Json frames から、呼び出したアイテムをループさせてるけど、k,v がどんな値をとりうるか予測できない
-    for k, v in js['frames'].items():
+    path = "json/new_json_data"
+    raw_json_list = glob(path + '/*')
 
-        # ここで、取り出して代入した値を確認する
-        print('k: ' + str(k) + ':' + 'v: ' + str(v))
-
-        k = int(k)
-
-        # Json 形式のlineを定義
-        line = {}
-
-        # .以下の形式を変換してみる(ビルドされなかったら)
-        line['file'] = '{0:04d}'.format(k + 1) + '.png'
-
-        # depthは、検知したいもしくは学習させる種類の数だと仮定する
-        line['image_size'] = [{
-            'width':int(v[0]['width']),
-            'height':int(v[0]['height']),
-            'depth':1
-        }]
-
-        # key 'image_size' で保存する valueを確認する
-        print('image_size width: ' + str(v[0]['width']))
-        print('image_size height: ' + str(v[0]['height']))
-
-        # ラベリングした情報を保存するために新たにリストを作成する
-        line['annotations'] = []
-
-        for annotation in v:
-
-            line['annotations'].append(
-                {
-                    'class_id':class_list[annotation['tags'][0]],
-                    'top':int(annotation['y1']),
-                    'left':int(annotation['x1']),
-                    'width':int(annotation['x2']) - int(annotation['x1']),
-                    'height':int(annotation['y2']) - int(annotation['y1'])
-                }
-            )
-
-            # 構成要素の確認を行う
-            print('class_id: ' + str(class_list[annotation['tags'][0]]))
-            print('top: ' + str(annotation['y1']))
-            print('left:' + str(annotation['x1']))
-            print('width: ' + str(int(annotation['x2']) - int(annotation['x1'])))
-            print('height: ' + str(int(annotation['y2']) - int(annotation['y1'])))
-
-        # ラベリングした情報を保存するために新たにリストを作成する
-        line['categories'] = []
-
-        for name, class_id in class_list.items():
-
-            line['categories'].append(
-                {
-                    'class_id':class_id,
-                    'name':name
-                }
-            )
-
-            # 構成要素の確認を行う
-            print('class_id: ' + str(class_id))
-            print('name: ' + str(name))
-
-        # 1つずつJsonファイルとして保存している
-        # w で指定すると、ファイルの新規書き込みが行われる
-        f = open('./json/raw' + '{0:04d}'.format(k + 1) + '.json', 'w')
-
-        # 定義したものをJson Format に対応させる
-        json.dump(line, f)
+    # os.rename('変更前のファイル名', '変更後のファイル名')
+    for i, f in enumerate(raw_json_list, 1):
+        root, ext = os.path.splitext(f)
+        os.rename(f, os.path.join(path, '{0:04d}').format(i) + ext)
